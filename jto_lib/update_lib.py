6d38ee0245b3da4217595246993b124f83207d2d
Sergei Konovalov <l0kifs91@gmail.com>
2022-09-19 22:03:35 +0300

    Change update_lib.py script

--- jto_lib/update_lib.py
+++ jto_lib/update_lib.py
@@ -4,7 +4,7 @@ import subprocess
 import sys
 
 
-working_dir = '.'
+working_dir = os.path.dirname(os.path.abspath(__file__))
 lib_name = 'jto'
 
 
@@ -14,13 +14,8 @@ for filename in os.listdir(working_dir):
         print(f'Remove directory {filename}')
         shutil.rmtree(filename)
 
-print('Build library')
-subprocess.Popen([sys.executable, os.path.join(working_dir, 'setup.py'), "bdist_wheel"]).wait()
-
 print(f'Unistall current version of library from interpreter')
 subprocess.Popen(["pip", "uninstall", lib_name, "-y"]).wait()
 
 print(f'Install library to current interpreter')
-all_files = os.listdir(os.path.join(working_dir, 'dist'))
-matching_file = [file for file in all_files if file.find('.whl')][0]
-subprocess.Popen(["pip", "install", os.path.join(working_dir, 'dist', matching_file)]).wait()
+subprocess.Popen(["pip", "install", working_dir]).wait()
